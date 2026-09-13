400ab077e83d3a848fc6a1cfcd9f24fce1231135
Nilsuspor <135379196+Nilsuspor@users.noreply.github.com>
2026-09-13 16:13:26 +0200

    created e2e tests amd added vercel.json

--- src/__tests__/eve/video.api.tests.ts
+++ src/__tests__/eve/video.api.tests.ts
@@ -1,8 +1,12 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { Resolutions } from '../../videos/types/videos';
+
 
 describe('/videos', () => {
-  beforeEach(async () => {
+
+  let createdVideoId: number;
+  beforeAll(async () => {
     await request(app)
       .delete('/testing/all-data')
       .expect(204);
@@ -13,4 +17,75 @@ describe('/videos', () => {
       .get('/videos')
       .expect(200, []);
   });
+
+
+  it('should create a video and return 201',async()=>
+  {
+    const res = await request(app)
+    .post('/videos/')
+    .send({ title: 'Поедание каши под дулом автомата',
+        author: 'Егор Зассульский',
+        availableResolutions: ['P720'] })
+    .expect(201);
+    createdVideoId = res.body.id;
+  }) 
+
+ it('should return 404', async()=>{
+    const res = await request(app)
+    .put(`/videos/99999`)
+    .send({
+        title: 'Неудачный конкурс с кашей',
+    author: "Проссульский",
+    availableResolutions: ['P1080'],
+    canBeDownloaded: true,
+    minAgeRestriction: 18,
+    publicationDate: new Date().toISOString()
+    })
+    .expect(404)
+  })
+
+
+ it('should return 400', async()=>{
+    const res = await request(app)
+    .put(`/videos/${createdVideoId}`)
+    .send({
+      title: 'Неудачный конкурс с кашей',
+    author: "Проссульский",
+    availableResolutions: ['P10810'],
+    canBeDownloaded: true,
+    minAgeRestriction: 18,
+    publicationDate: new Date().toISOString()
+    })
+    .expect(400)
+  })
+
+
+
+  it('should update video by ID and return 204', async()=>{
+    const updateData = {
+      title: 'Неудачный конкурс с кашей',
+    author: "Проссульский",
+    availableResolutions: ['P1080'],
+    canBeDownloaded: true,
+    minAgeRestriction: 18,
+    publicationDate: new Date().toISOString()
+    }
+    const res = await request(app)
+    .put(`/videos/${createdVideoId}`)
+    .send(updateData)
+    .expect(204)
+
+    const getRes = await request(app)
+      .get(`/videos/${createdVideoId}`)
+      .expect(200);
+    expect(getRes.body.title).toBe(updateData.title);
+    expect(getRes.body.author).toBe(updateData.author);
+    expect(getRes.body.availableResolutions).toEqual(updateData.availableResolutions);
+    expect(getRes.body.canBeDownloaded).toBe(updateData.canBeDownloaded);
+    expect(getRes.body.minAgeRestriction).toBe(updateData.minAgeRestriction);
+    expect(getRes.body.publicationDate).toBe(updateData.publicationDate);
+
+  })
+
+
 });
